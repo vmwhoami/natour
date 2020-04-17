98e420dcf9ebb3ffae1393837b7f2be426b6eda9
vmwhoami <vmwhoami@gmail.com>
2020-04-17 16:18:36 +0300

    Aded tour models

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,40 @@ const tourSchema = new mongoose.Schema({
     type: String,
     require: [true, "E nevoie de un nume!!!"],
     unique: true,
+  }, duration: {
+    type: Number,
+    required: [true, "Please specify the duration of the tourS"]
+  }, maxGroupSize: {
+    type: Number,
+    required: [true, "Please let us know what is the max group size"]
   },
-  rating: {
+  difficulty: {
+    type: String,
+  },
+  ratingsAverage: {
     type: Number,
     default: 3.4,
+  },
+  ratingsQuantity: {
+    type: Number
   },
   price: {
     type: Number,
     required: [true, " E nevoie de un pret!!!"],
   },
+  summary: {
+    type: String,
+    required: [true, "We need a small description of the tour"]
+  },
+  description: {
+    type: String
+  },
+  imageCover: {
+    type: String,
+    required: [true, "It is needed the image of the cover"]
+  },
+  images: [String],
+  startDates: [Date]
 });
 const Tour = mongoose.model("Tour", tourSchema);
 
